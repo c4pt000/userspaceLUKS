02aa143fdfd1118cc688fe4ac8f2dad5fe13b630
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2019-06-10 11:58:17 -0400

    Update init-builder.sh

--- init-builder.sh
+++ init-builder.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+export LDFLAGS="-L/usr/local/opt/openssl/lib"
+export CPPFLAGS="-I/usr/local/opt/openssl/include"
+export PKG_CONFIG_PATH="/usr/local/opt/openssl/lib/pkgconfig"
+
 autoreconf --install
 autoconf 
 ./configure 
